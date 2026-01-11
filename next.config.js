83c7dbc951b0e7e05f425387b00de774592dc7e2
omarhkhamis <omar95net@gmail.com>
2026-01-11 22:09:47 +0100

    third

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/dental-implant',
+
+  i18n: {
+    locales: ['en', 'ru'],
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
